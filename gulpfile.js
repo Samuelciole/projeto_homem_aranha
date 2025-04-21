0ef3efab1cee57e5f85f792a38fd5c419ba98dab
samuel <samuelciole@gmail.com>
2025-04-21 10:42:23 -0300

    subindo html para dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 
+function html() {
+  return gulp.src('./*.html') // pega todos os .html da raiz
+    .pipe(gulp.dest('dist'));
+}
+
+
 
 
 function scripts(){
@@ -22,4 +28,4 @@ function watchFiles() {
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
   }
 
-exports.default = gulp.parallel(styles, watchFiles, scripts);
+exports.default = gulp.parallel(styles, watchFiles, scripts, html);
